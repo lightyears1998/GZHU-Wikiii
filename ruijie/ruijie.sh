d641dda9be5ca23104253d08893f25aa32ebc17c
axionl <axionl@aosc.io>
2017-09-10 11:31:46 +0800

    fix ruijie.sh

--- ruijie/ruijie.sh
+++ ruijie/ruijie.sh
@@ -8,18 +8,17 @@ do
 		echo "success"
 	else
 		echo "failed & try to connecting ..."
-        pid=`ps -aux | grep rjsupplicant | grep -v grep | grep -o -w "^[0-9]*" | tail -n1`
+        pid=`ps -aux | grep rjsupplicant.sh | grep -v grep | grep -o -w "^[0-9]*" | head -n 1`
         if test -z "$pid"; then                                                                                                                              
 ~/Documents/CMOS-AO
             echo "empty"
-            /usr/bin/bash /usr/bin/rjsupplicant/rjsupplicant.sh -d 1 -u 1519400067 -p 
-heyitonglaila4
+            screen /usr/bin/bash /usr/bin/rjsupplicant/rjsupplicant.sh -d 1 -u <id> -p <password>
         else
             echo "Killing the rjsupplicant pid=$pid ..."
             kill -KILL $pid
         fi
 
     fi
-	sleep 3
+	sleep 1
 done
 
